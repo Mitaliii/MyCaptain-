91f24ab7984444165eeefeeb2c75f946d8e65b5b
Mitaliii <66302933+Mitaliii@users.noreply.github.com>
2021-01-13 17:04:49 +0530

    Update Positive nums in a list.py

--- Positive nums in a list.py
+++ Positive nums in a list.py
@@ -1,4 +1,13 @@
-from collections import Counter
-word=str(input("Please enter a string "))
-count=Counter(word)
-print(str(count))
+list1=[12,-7,5,64,-14]
+lista1=[]
+list2=[12,14,-95,3]
+lista2=[]
+for num in list1:
+    if num>=0:
+        lista1.append(num)
+print(lista1)
+for num in list2:
+    if num>=0:
+        lista2.append(num)
+print(lista2)   
+   
